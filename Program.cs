ec508781c61317c547e99fe2afe8754b58a0eb51
Andreh Marques <andrehmarquez@gmail.com>
2025-03-12 09:37:02 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Pressione uma tecla para começar a contagem regressiva!");
+Console.ReadKey();
+
+
+Console.WriteLine("Preparar...");
+Console.ReadKey();
+
+
+Console.WriteLine("Apontar...");
+Console.ReadKey();
+
+
+Console.WriteLine("FOGO!");
+
+
